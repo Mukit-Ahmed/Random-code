90c357d6dd08b4d6b8ed4f5891a5dbde81f02898
Mukit Ahmed <mukitahmed1933@gmail.com>
2025-10-15 18:29:13 +0600

    Add files via upload

---
+++ Graph theory/DFS/depthFirstSearch.cpp
@@ -0,0 +1,35 @@
+void dfs(vector<int> &components,int node,vector<bool> &visited,vector<vector<int>> &adj){
+
+components.push_back(node);
+visited[node]=1;
+for(auto i:adj[node] ){
+    if(!visited[i]){
+        dfs(components,i,visited,adj);
+    }
+}
+
+}
+
+
+vector<vector<int>> depthFirstSearch(int V, int E, vector<vector<int>> &edges)
+{
+vector<vector<int>> adj(V);
+for(int i = 0 ;i<E;i++){
+    adj[edges[i][0]].push_back(edges[i][1]);
+    adj[edges[i][1]].push_back(edges[i][0]);
+}
+
+vector<bool> visited(V,0);
+vector<vector<int>> ans;
+for(int i=0;i<V;i++){
+vector<int> components;
+if(!visited[i]){
+ dfs(components,i,visited,adj);
+ ans.push_back(components);
+}
+
+}
+
+    return ans;
+
+}
